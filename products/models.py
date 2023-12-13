9443783b0c344fe8a5208c96104713525b794b2a
FARSHAD-SH <farshad.shamlu@gmail.com>
2023-12-13 17:24:21 +0100

    trydjango first step2

--- products/models.py
+++ products/models.py
@@ -5,5 +5,5 @@ class Product(models.Model):
     title       = models.CharField(max_length=120) # max_length = required
     description = models.TextField(blank=True, null=True)
     price       = models.DecimalField(decimal_places=2, max_digits=10000)
-    summary     = models.TextField(blank=False, null=False, default='This is cool!')
+    summary     = models.TextField(blank=False, null=False, default='Thiss is cool!')
     featured    = models.BooleanField(default=False) # null=True, default=True
