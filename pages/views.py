f0c42309f876929daab82dfb7c3efd0e2750f54f
FARSHAD-SH <farshad.shamlu@gmail.com>
2023-12-15 16:07:43 +0100

    trydjango templates

--- pages/views.py
+++ pages/views.py
@@ -5,18 +5,19 @@ from django.shortcuts import render
 def home_view(request, *args, **kwargs): # *args, **kwargs
     print(args, kwargs)
     print(request.user)
-    return HttpResponse("<h1>Hello World</h1>") # string of HTML code
-    # return render(request, "home.html", {})
+    # return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+    return render(request, "home.html", {})
 
 def contact_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>")
+    # return HttpResponse("<h1>Contact Page</h1>")
+    return render(request, "contact.html", {})
 
 def about_view(request, *args, **kwargs):
-    return HttpResponse("<h1>About Page</h1>")
-    # my_context = {
-    #     "my_text": "This is about us",
-    #     "my_number": 123,
-    #     "my_list": [123, 4242, 12313, "Abc"]
-    # }
-    # return render(request, "about.html", my_context)
+    # return HttpResponse("<h1>About Page</h1>")
+    my_context = {
+        "my_text": "This is about us",
+        "my_number": 123,
+        "my_list": [123, 4242, 12313, "Abc"]
+    }
+    return render(request, "about.html", my_context)
 
--- trydjango/settings.py
+++ trydjango/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -78,7 +80,7 @@ ROOT_URLCONF = 'trydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,7 +102,8 @@ WSGI_APPLICATION = 'trydjango.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
