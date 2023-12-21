a2fba16674bbd42593d25bed3214c24fc29a1cae
FARSHAD-SH <farshad.shamlu@gmail.com>
2023-12-21 10:03:58 +0100

    trydjango OR query

--- student/views.py
+++ student/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 from .models import Teacher, Student
+from django.db import connection
+from django.db.models import Q
 
 # Create your views here.
 def student_list_view(request):
 
-    obj = Student.objects.get(id=1)
-    context = {
-        'object': obj
-    }
+    # obj = Student.objects.get(id=1)
+    # context = {
+    #     'object': obj
+    # }
+    posts = Student.objects.all()
+    print(posts)
+    print(type(posts))
+    print(posts.query)
+    print(connection.queries)
 
-    return render(request, "student/list.html", context)
+
+    return render(request, "student/list.html", {'posts': posts})
+
+def student_list_view(request):
+    # posts = Student.objects.filter(surname__startswith='danesh') | Student.objects.filter(surname__startswith='shamlu')
+    posts = Student.objects.filter(~Q(surname__startswith='danesh') | Q (surname__startswith='shamlu') | Q (surname__startswith='fasihiani'))
+
+    print(posts)
+    print(connection.queries)
+
+    return render(request, "student/list.html", {'posts': posts})
